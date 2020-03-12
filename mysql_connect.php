894a61f81f4e03b442cd776563a695e6bca81aec
SmthNameHere <38471186+SmthNameHere@users.noreply.github.com>
2020-03-12 02:48:23 +0200

    Update Database connection

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,8 +1,8 @@
 <?php
-$user = 'root';
-$password = 'root';
-$db = 'testing';
-$host = 'localhost';
+$user = 'f4udc2gb5709weq4';
+$password = 'prboznh0jti1en8l';
+$db = 'umvhqlt2degpuufz';
+$host = 'nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
 
 $dsn = 'mysql:host='.$host.';dbname='.$db;
 $pdo = new PDO($dsn,$user,$password);
